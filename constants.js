72f8af8c62dffd2f68d3ba11e6057122737757d7
Shebin <sv@tectusdreamlab.com>
2022-04-22 13:34:36 +0800

    updated to pdfbox version 2.0.26 since 2.0.25 no longer available for download - readme and constants

--- constants.js
+++ constants.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
 
-  VENDOR_PDF_BOX_JAR: 'pdfbox-app-2.0.25.jar',
+  VENDOR_PDF_BOX_JAR: 'pdfbox-app-2.0.26.jar',
   VENDOR_TIKA_JAR: 'tika-app-2.3.0.jar',
 
   DIRECTORY: {
